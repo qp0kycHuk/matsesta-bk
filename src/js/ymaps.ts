dd49824dfc98c321e22559be1e04718105681d67
qp0kycHuk <serega990112@mail.ru>
2023-12-12 11:08:01 +0300

    add reviews

--- src/js/ymaps.ts
+++ src/js/ymaps.ts
@@ -12,36 +12,38 @@ function mapsInit() {
   if (mapInited) return
   mapInited = true
 
-  ymaps.load('https://api-maps.yandex.ru/2.1/?lang=ru_RU').then((maps: any) => {
-    const coords = window.coords
-    const center = [coords[0], coords[1]]
+  if (document.getElementById('map')) {
+    ymaps.load('https://api-maps.yandex.ru/2.1/?lang=ru_RU').then((maps: any) => {
+      const coords = window.coords
+      const center = [coords[0], coords[1]]
 
-    const map = new maps.Map('map', {
-      center: center,
-      zoom: 16,
+      const map = new maps.Map('map', {
+        center: center,
+        zoom: 16,
+      })
+
+      const placemark = new maps.Placemark(
+        coords,
+        {},
+        {
+          iconLayout: 'default#image',
+          iconImageHref: '../img/geo.svg',
+          iconImageSize: [74, 84],
+          iconImageOffset: [-(74 / 2), -84],
+        }
+      )
+
+      map.controls.remove('geolocationControl')
+      map.controls.remove('searchControl')
+      map.controls.remove('trafficControl')
+      map.controls.remove('typeSelector')
+      map.controls.remove('fullscreenControl')
+      // map.controls.remove('zoomControl')
+      map.controls.remove('rulerControl')
+      map.behaviors.disable(['scrollZoom'])
+      map.geoObjects.add(placemark)
     })
-
-    const placemark = new maps.Placemark(
-      coords,
-      {},
-      {
-        iconLayout: 'default#image',
-        iconImageHref: '../img/geo.svg',
-        iconImageSize: [74, 84],
-        iconImageOffset: [-(74 / 2), -84],
-      }
-    )
-
-    map.controls.remove('geolocationControl')
-    map.controls.remove('searchControl')
-    map.controls.remove('trafficControl')
-    map.controls.remove('typeSelector')
-    map.controls.remove('fullscreenControl')
-    // map.controls.remove('zoomControl')
-    map.controls.remove('rulerControl')
-    map.behaviors.disable(['scrollZoom'])
-    map.geoObjects.add(placemark)
-  })
+  }
 }
 
 export default { init }
